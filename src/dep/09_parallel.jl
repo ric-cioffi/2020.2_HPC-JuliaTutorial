6911c7ba8bce254cb8ba6c1150ac8cc0a30b75e3
Riccardo Cioffi <ric.cioffi90@gmail.com>
2020-09-04 12:56:32 +0200

    fix 09_parallel.jl
    Signed-off-by: Riccardo Cioffi <ric.cioffi90@gmail.com>

--- src/dep/09_parallel.jl
+++ src/dep/09_parallel.jl
@@ -19,7 +19,7 @@ addprocs(3)
 #--------------------------------#
 
 # Data structure of state and exogenous variables
-@everywhere struct modelState
+@everywhere struct ModelStateShared
   ind::Int64
   ne::Int64
   nx::Int64
@@ -35,8 +35,144 @@ addprocs(3)
   r::Float64
 end
 
-# Function that computes value function, given vector of state variables
-@everywhere function value(currentState::modelState)
+@everywhere struct ModelStatePMap
+  ind::Int64
+  ne::Int64
+  nx::Int64
+  T::Int64
+  age::Int64
+  P::Array{Float64,2}
+  xgrid::Vector{Float64}
+  egrid::Vector{Float64}
+  ssigma::Float64
+  bbeta::Float64
+  V::Array{Float64,2}
+  w::Float64
+  r::Float64
+end
+
+struct ModelStateThreads
+  ind::Int64
+  ne::Int64
+  nx::Int64
+  T::Int64
+  age::Int64
+  P::Array{Float64,2}
+  xgrid::Vector{Float64}
+  egrid::Vector{Float64}
+  ssigma::Float64
+  bbeta::Float64
+  V::Array{Float64,2}
+  w::Float64
+  r::Float64
+end
+
+# Function that computes value_shared function, given vector of state variables
+@everywhere function value_shared(currentState::ModelStateShared)
+
+  ind     = currentState.ind
+  age     = currentState.age
+  ne      = currentState.ne
+  nx      = currentState.nx
+  T       = currentState.T
+  P       = currentState.P
+  xgrid   = currentState.xgrid
+  egrid   = currentState.egrid
+  ssigma  = currentState.ssigma
+  bbeta   = currentState.bbeta
+  w       = currentState.w
+  r       = currentState.r
+  V       = currentState.V
+
+  ix      = convert(Int, floor((ind-0.05)/ne))+1;
+  ie      = convert(Int, floor(mod(ind-0.05, ne))+1);
+
+  VV      = -10.0^3;
+  ixpopt  = 0;
+
+
+    for ixp = 1:nx
+
+      expected = 0.0;
+      if(age < T)
+        for iep = 1:ne
+          expected = expected + P[ie, iep]*V[ixp, iep];
+        end
+      end
+
+      cons  = (1 + r)*xgrid[ix] + egrid[ie]*w - xgrid[ixp];
+
+      utility = (cons^(1-ssigma))/(1-ssigma) + bbeta*expected;
+
+      if(cons <= 0)
+        utility = -10.0^(5);
+      end
+
+      if(utility >= VV)
+        VV = utility;
+        ixpopt = ixp;
+      end
+
+      utility = 0.0;
+    end
+
+    return(VV);
+
+end
+
+@everywhere function value_pmap(currentState::ModelStatePMap)
+
+  ind     = currentState.ind
+  age     = currentState.age
+  ne      = currentState.ne
+  nx      = currentState.nx
+  T       = currentState.T
+  P       = currentState.P
+  xgrid   = currentState.xgrid
+  egrid   = currentState.egrid
+  ssigma  = currentState.ssigma
+  bbeta   = currentState.bbeta
+  w       = currentState.w
+  r       = currentState.r
+  V       = currentState.V
+
+  ix      = convert(Int, floor((ind-0.05)/ne))+1;
+  ie      = convert(Int, floor(mod(ind-0.05, ne))+1);
+
+  VV      = -10.0^3;
+  ixpopt  = 0;
+
+
+    for ixp = 1:nx
+
+      expected = 0.0;
+      if(age < T)
+        for iep = 1:ne
+          expected = expected + P[ie, iep]*V[ixp, iep];
+        end
+      end
+
+      cons  = (1 + r)*xgrid[ix] + egrid[ie]*w - xgrid[ixp];
+
+      utility = (cons^(1-ssigma))/(1-ssigma) + bbeta*expected;
+
+      if(cons <= 0)
+        utility = -10.0^(5);
+      end
+
+      if(utility >= VV)
+        VV = utility;
+        ixpopt = ixp;
+      end
+
+      utility = 0.0;
+    end
+
+    return(VV);
+
+end
+
+function value_threads(currentState::ModelStateThreads)
 
   ind     = currentState.ind
   age     = currentState.age
@@ -116,11 +252,11 @@ function main_shared()
     @everywhere egrid = zeros(ne)
     @everywhere P     = zeros(ne, ne)
 
-    # Initialize value function V
+    # Initialize value_shared function V
     @everywhere V          = zeros(T, nx, ne)
     @everywhere V_tomorrow = zeros(nx, ne)
 
-    # Initialize value function as a shared array
+    # Initialize value_shared function as a shared array
     tempV = SharedArray{Float64}(ne*nx)
 
     #--------------------------------#
@@ -183,8 +319,8 @@ function main_shared()
         ix      = convert(Int, ceil(ind/ne));
         ie      = convert(Int, floor(mod(ind-0.05, ne))+1);
 
-        currentState = modelState(ind,ne,nx,T,age,P,xgrid,egrid,ssigma,bbeta, V_tomorrow,w,r)
-        tempV[ind] = value(currentState);
+        currentState = ModelStateShared(ind,ne,nx,T,age,P,xgrid,egrid,ssigma,bbeta, V_tomorrow,w,r)
+        tempV[ind] = value_shared(currentState);
 
       end
 
@@ -197,16 +333,14 @@ function main_shared()
         V_tomorrow[ix, ie] = tempV[ind]
       end
 
-      finish = convert(Int, Dates.value(Dates.unix2datetime(time())- start))/1000;
+      finish = convert(Int, Dates.value_shared(Dates.unix2datetime(time())- start))/1000;
       print("Age: ", age, ". Time: ", finish, " seconds. \n")
     end
 
     print("\n")
-    finish = convert(Int, Dates.value(Dates.unix2datetime(time())- start))/1000;
+    finish = convert(Int, Dates.value_shared(Dates.unix2datetime(time())- start))/1000;
     print("TOTAL ELAPSED TIME: ", finish, " seconds. \n")
 end
-
-
 
 function main_pmap()
 
@@ -237,7 +371,7 @@ function main_pmap()
     egrid = zeros(ne)
     P     = zeros(ne, ne)
 
-    # Initialize value function V
+    # Initialize value_pmap function V
     V           = zeros(T, nx, ne)
     V_tomorrow  = zeros(nx, ne)
 
@@ -294,9 +428,9 @@ function main_pmap()
 
     for age = T:-1:1
 
-      pars = [modelState(ind,ne,nx,T,age,P,xgrid,egrid,ssigma,bbeta, V_tomorrow,w,r) for ind in 1:ne*nx];
+      pars = [ModelStatePMap(ind,ne,nx,T,age,P,xgrid,egrid,ssigma,bbeta, V_tomorrow,w,r) for ind in 1:ne*nx];
 
-      s = pmap(value,pars)
+      s = pmap(value_pmap,pars)
 
       for ind = 1:nx*ne
         ix      = convert(Int, floor((ind-0.05)/ne))+1;
@@ -306,16 +440,15 @@ function main_pmap()
         V_tomorrow[ix, ie] = s[ind]
       end
 
-      finish = convert(Int, Dates.value(Dates.unix2datetime(time())- start))/1000;
+      finish = convert(Int, Dates.value_pmap(Dates.unix2datetime(time())- start))/1000;
       print("Age: ", age, ". Time: ", finish, " seconds. \n")
 
     end
 
     print("\n")
-    finish = convert(Int, Dates.value(Dates.unix2datetime(time())- start))/1000;
+    finish = convert(Int, Dates.value_pmap(Dates.unix2datetime(time())- start))/1000;
     print("TOTAL ELAPSED TIME: ", finish, " seconds. \n")
 end
-
 
 function main_threads()
 
@@ -346,11 +479,11 @@ function main_threads()
      egrid = zeros(ne)
      P     = zeros(ne, ne)
 
-    # Initialize value function V
+    # Initialize value_threads function V
      V          = zeros(T, nx, ne)
      V_tomorrow = zeros(nx, ne)
 
-    # Initialize value function as a shared array
+    # Initialize value_threads function as a shared array
     tempV = zeros(ne*nx)
 
     #--------------------------------#
@@ -409,8 +542,8 @@ function main_threads()
         ix      = convert(Int, ceil(ind/ne));
         ie      = convert(Int, floor(mod(ind-0.05, ne))+1);
 
-        currentState = modelState(ind,ne,nx,T,age,P,xgrid,egrid,ssigma,bbeta, V_tomorrow,w,r)
-        tempV[ind] = value(currentState);
+        currentState = ModelStateThreads(ind,ne,nx,T,age,P,xgrid,egrid,ssigma,bbeta, V_tomorrow,w,r)
+        tempV[ind] = value_threads(currentState);
 
       end
 
@@ -423,11 +556,11 @@ function main_threads()
         V_tomorrow[ix, ie] = tempV[ind]
       end
 
-      finish = convert(Int, Dates.value(Dates.unix2datetime(time())- start))/1000;
+      finish = convert(Int, Dates.value_threads(Dates.unix2datetime(time())- start))/1000;
       print("Age: ", age, ". Time: ", finish, " seconds. \n")
     end
 
     print("\n")
-    finish = convert(Int, Dates.value(Dates.unix2datetime(time())- start))/1000;
+    finish = convert(Int, Dates.value_threads(Dates.unix2datetime(time())- start))/1000;
     print("TOTAL ELAPSED TIME: ", finish, " seconds. \n")
 end
